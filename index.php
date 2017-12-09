a2264d5c08d027ef6542bb150511cdb8a0df6a74
angelobagasbas <33994486+angelobagasbas@users.noreply.github.com>
2017-12-09 14:13:12 +0800

    change title tag

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Make a guess</title>
+    <title>Bagasbas, Angelo Dan F.</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
